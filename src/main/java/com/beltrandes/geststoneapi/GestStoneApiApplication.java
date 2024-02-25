0fd940ed7c480332e5f0731b81d6b4560c9fcb3b
Beltrandes <beltsmarques@hotmail.com>
2024-02-24 22:09:36 -0300

    global CORS configuration implemented

--- src/main/java/com/beltrandes/geststoneapi/GestStoneApiApplication.java
+++ src/main/java/com/beltrandes/geststoneapi/GestStoneApiApplication.java
@@ -2,12 +2,27 @@ package com.beltrandes.geststoneapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class GestStoneApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestStoneApiApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 }
